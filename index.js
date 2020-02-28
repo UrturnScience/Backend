7e445975faedb4b0358e6f3ac62ded8bb91dde0c
Kevin Tang <kevin26428@gmail.com>
2020-02-28 15:06:12 -0600

    Setup DB connection to MongoDB (#9)
    * removed unecessary example
    
    * more clear naming

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 require("dotenv").config();
 
+const { setupDB } = require("./src/services/dbSetup");
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
+
+setupDB().then(() => {
+  console.log("Connected to Database");
+});
 
 app.get("/", (req, res) =>
   res.send("Hello World! This app was deployed automatically.")
