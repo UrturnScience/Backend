554d767e6dc45967e2e6e9f50f1c25b47923856b
Kevin Tang <kevin26428@gmail.com>
2020-03-02 12:46:56 -0600

    added middleware

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 
 const express = require("express");
+const errorMiddleware = require("./src/errorMiddleware");
 const app = express();
 const port = 3000;
+
+app.use(errorMiddleware);
 
 app.get("/", (req, res) =>
   res.send("Hello World! This app was deployed automatically.")
