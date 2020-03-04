c24d802c5a0fc1b5dc690b3afeec08e652e52b63
shaneponcik <41391818+shaneponcik@users.noreply.github.com>
2020-03-04 13:54:55 -0600

    added proper response for each of the user routes (#18)
    * added proper response for each of the routes
    
    * send status

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,25 +7,25 @@ exports.create = async function(req, res) {
   });
 
   await user.save();
-  res.status(200).send("User created successfully: " + user.id);
+  res.status(200).json({ _id: user.id });
 };
 
 exports.show_all = async function(req, res) {
   const users = await User.find({});
-  res.status(200).send(users);
+  res.status(200).json({ users });
 };
 
 exports.details = async function(req, res) {
   const user = await User.findById(req.params.id);
-  res.status(200).send(user);
+  res.status(200).json({ user });
 };
 
 exports.update = async function(req, res) {
   const user = await User.findByIdAndUpdate(req.params.id, { $set: req.body });
-  res.status(200).send(user);
+  res.status(200).json({ user });
 };
 
 exports.delete = async function(req, res) {
   await User.findByIdAndDelete(req.params.id);
-  res.status(200).send("User Deleted with ID:" + req.params.id);
+  res.sendStatus(200);
 };
