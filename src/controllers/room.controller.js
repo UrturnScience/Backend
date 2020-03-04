4aa5c5fcf603d48c39040223ecd693da2179725d
Shane Poncik <sbponcik@gmail.com>
2020-03-04 13:51:45 -0600

    converted room controller's functions to async

--- src/controllers/room.controller.js
+++ src/controllers/room.controller.js
@@ -1,79 +1,48 @@
 const Room = require("../models/room.model");
 const User = require("../models/user.model");
 
-exports.create = function(req, res) {
+exports.create = async function(req, res) {
   const room = new Room({
     active: req.body.active
   });
 
-  room.save(function(err) {
-    if (err) {
-      res.status(500).send("Error");
-    } else {
-      res.status(200).send("Room created successfully: " + room.id);
-    }
-  });
+  await room.save();
+  res.status(200).send("Room created successfully: " + room.id);
 };
 
-exports.show_all = function(req, res) {
-  Room.find({}, (err, rooms) => {
-    if (err) {
-      res.status(500).send("Error");
-    } else {
-      res.status(200).send(rooms);
-    }
-  });
+exports.show_all = async function(req, res) {
+  const rooms = await Room.find({});
+  res.status(200).send(rooms);
 };
 
-exports.details = function(req, res) {
-  Room.findById(req.params.id, (err, room) => {
-    if (err) {
-      res.status(500).send("Error");
-    } else {
-      res.status(200).send(room);
-    }
-  });
+exports.details = async function(req, res) {
+  constroom = await Room.findOne({ _id: req.params.id });
+  res.status(200).send(room);
 };
 
-exports.update = function(req, res) {
-  Room.findOneAndUpdate(
-    { _id: req.params.id },
-    { $set: req.body },
-    { new: true },
-    (err, room) => {
-      if (err) {
-        res.status(500).send("Error");
-      } else {
-        res.status(200).send(room);
-      }
-    }
-  );
+exports.update = async function(req, res) {
+  const room = await Room.findOneAndUpdate({ _id: req.params.id },{ $set: req.body }, {new: true});
+  res.status(200).send(room);
 };
 
-exports.delete = function(req, res) {
-  Room.findByIdAndDelete(req.params.id, err => {
-    if (err) {
-      res.status(500).send("Error");
-    } else {
-      res.status(200).send("room Deleted with ID:" + req.params.id);
-    }
-  });
+exports.delete = async function(req, res) {
+  await Room.findByIdAndDelete(req.params.id);
+  res.status(200).json({_id: req.params.id});
 };
 
 exports.add_user = async function(req, res) {
-  const room = await Room.findById(req.params.rid);
-  const user = await User.findById(req.params.uid);
+  const [room, user] = await Promise.all([Room.findOne({_id:req.params.rid}), User.findOne({_id:req.params.uid})]);
 
   await room.addUser(user._id);
   await user.addRoom(room._id);
-  res.status(200).send("Complete");
+
+  res.sendStatus(200);
 }
 
 exports.remove_user = async function(req, res){
-  const [room, user] = await Promise.all([Room.findById(req.params.rid), User.findById(req.params.uid)]);
-  console.log(1);
+  const [room, user] = await Promise.all([Room.findOne({_id:req.params.rid}), User.findOne({_id:req.params.uid})]);
+
   await room.removeUser(user._id);
   await user.removeRoom(room._id);
-  console.log(2);
-  res.status(200).send("Removed user from room");
+  res.sendStatus(200);
 }
