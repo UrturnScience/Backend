fc14d29ef12059d4c0020b337a15b4ca9a0cba28
Shane Poncik <sbponcik@gmail.com>
2020-03-07 14:55:02 -0600

    fixed room_user route statement, made user.username unique

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 let UserSchema = new Schema({
-  username: { type: String, required: true, max: 100 },
+  username: { type: String, required: true, max: 100, unique: true },
   password: { type: String, required: true, max: 25 }
 });
 
