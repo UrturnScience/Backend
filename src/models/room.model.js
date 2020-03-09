1614de57855cd8b61a5916af9039e90dc35e00d1
shaneponcik <41391818+shaneponcik@users.noreply.github.com>
2020-03-09 12:29:20 -0500

    Migrated to RoomUser bridge table (#22)
    * Added RoomUser model, and altered Room and User to not have arrays
    
    * Created routers for room_user, and removed routers frm room(add, etc.)
    
    * room_user routes to index, and changed ru.show_room route endpoint
    
    * removed room's add/remUser, create room_user controller, chng ru model
    
    * app.use(RoomUserRoutes)
    
    * fixed room_user require statement
    
    * fixed room_user route statement, made user.username unique
    
    * fixed compound key on roomuser, added roomuser.show_user(rooms for user)
    
    * prettified files
    
    * Changes in RoomUser async functions(per PR)

--- src/models/room.model.js
+++ src/models/room.model.js
@@ -2,20 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 let RoomSchema = new Schema({
-  active: { type: Boolean, required: true, default: true },
-  users: [{type: Schema.Types.ObjectId, ref: "User"}]
+  active: { type: Boolean, required: true, default: true }
 });
-
-RoomSchema.methods.addUser = async function(userId){
-    if (this.users.includes(userId) == false){
-      await this.users.push(userId);
-      await this.save();
-    }
-}
-
-RoomSchema.methods.removeUser = async function(userId){
-  await this.users.remove(userId);
-  await this.save();
-}
 
 module.exports = mongoose.model("Room", RoomSchema);
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,21 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 let UserSchema = new Schema({
-  username: { type: String, required: true, max: 100 },
-  password: { type: String, required: true, max: 25 },
-  rooms: [{type: Schema.Types.ObjectId, ref: "Room"}]
+  username: { type: String, required: true, max: 100, unique: true },
+  password: { type: String, required: true, max: 25 }
 });
 
-UserSchema.methods.addRoom = async function(roomId){
-  if(this.rooms.includes(roomId) == false){
-    await this.rooms.push(roomId);
-    await this.save();
-  }
-}
-
-UserSchema.methods.removeRoom = async function(roomId){
-  await this.rooms.remove(roomId);
-  await this.save();
-}
 
 module.exports = mongoose.model("User", UserSchema);
