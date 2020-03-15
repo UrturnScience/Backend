274355b7eba4e9abf9e98440bb353530e0b04c9d
Kevin T <kdt0116@tamu.edu>
2020-03-15 12:00:21 -0500

    added firebaseId to model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-let UserSchema = new Schema({
+const UserSchema = new Schema({
   username: { type: String, required: true, max: 100, unique: true },
-  password: { type: String, required: true, max: 25 }
+  firebaseId: {
+    type: String,
+    required: true,
+    max: 128,
+    unique: true,
+    index: true
+  }
 });
-
 
 module.exports = mongoose.model("User", UserSchema);
