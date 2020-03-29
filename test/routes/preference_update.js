1522bf3f64cc7d20be4c1bfb4ae98fcb35b34543
shaneponcik <41391818+shaneponcik@users.noreply.github.com>
2020-03-29 17:28:57 -0500

    fixed update preference's checking(removed) (#41)
    * fixed update preference's checking(removed)
    
    * new line
    
    * added temporary fix for preferences in case breaks
    
    * test case, added user to room

--- test/routes/preference_update.js
+++ test/routes/preference_update.js
@@ -6,6 +6,7 @@ const create_models = require("../util/create_models");
 const PreferenceChecker = require("../util/preference_checker");
 
 const Preference = require("../../src/models/preference.model");
+const RoomUser = require("../../src/models/room_user.model");
 
 test.before(t => {});
 
@@ -26,6 +27,7 @@ test.serial("PUT /preference/update/:id/:weight", async t => {
   const room = await create_models.create_room();
   const chore1 = await create_models.create_chore(room.id, "Dishes", 5);
   const chore2 = await create_models.create_chore(room.id, "Laundry", 5);
+  await create_models.create_room_user(room.id, user.id);
 
   const preference1 = await create_models.create_preference(chore1.id, user.id, 0);
   const preference2 = await create_models.create_preference(chore2.id, user.id, 1);
@@ -42,7 +44,7 @@ test.serial("PUT /preference/update/:id/:weight", async t => {
   const updatedPreference1 = await Preference.findOne({_id: preference1.id});
   const updatedPreference2 = await Preference.findOne({_id: preference2.id});
 
-  t.truthy(updatedPreference1.weight == 1);
-  t.truthy(updatedPreference2.weight == 0);
-  t.truthy(await PreferenceChecker.validateUserPreferencesByRoomId(room.id));
+  t.true(updatedPreference1.weight == 1);
+  t.true(updatedPreference2.weight == 0);
+  t.true(await PreferenceChecker.validateUserPreferencesByRoomId(room.id));
 });
