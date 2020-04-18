da61094c38052252535cc974306bc54d5dcf1fa0
shaneponcik <41391818+shaneponcik@users.noreply.github.com>
2020-04-18 17:41:44 -0500

    Weekly Assignment Cycle(with Cron Job) (#67)
    * Added assignment cycle w/ cron job and tests for the function
    
    * Prettier files
    
    * Kevin's changes

--- src/controllers/assignment.controller.js
+++ src/controllers/assignment.controller.js
@@ -2,38 +2,49 @@ const Assignment = require("../models/assignment.model");
 
 const AssignmentService = require("../services/assignment.service");
 
-exports.create_assignments = async function(req, res){
+exports.assignment_cycle = async function (req, res) {
+  await AssignmentService.processAssignmentCycle();
+  res.sendStatus(200);
+};
+
+exports.create_assignments = async function (req, res) {
   await AssignmentService.createAssignments();
   res.sendStatus(200);
-}
+};
 
-exports.retire_assignments = async function(req, res){
+exports.retire_assignments = async function (req, res) {
   await AssignmentService.retireAssignments();
   res.sendStatus(200);
-}
+};
 
-exports.toggle_active = async function(req, res) {
+exports.toggle_active = async function (req, res) {
   //Should flip assignment's active status
-  const assignment = await Assignment.findOne({_id: req.params.id});
-  assignment.active = !(assignment.active);
+  const assignment = await Assignment.findOne({ _id: req.params.id });
+  assignment.active = !assignment.active;
   await assignment.save();
   res.sendStatus(200);
 };
 
-exports.details = async function(req, res) {
+exports.details = async function (req, res) {
   const assignment = await Assignment.findById(req.params.id);
   res.status(200).json({ assignment });
 };
 
-exports.active_user = async function(req, res){
-  const assignments = await Assignment.find({userId: req.params.uid, active: true});
-  res.status(200).json({assignments});
-}
+exports.active_user = async function (req, res) {
+  const assignments = await Assignment.find({
+    userId: req.params.uid,
+    active: true,
+  });
+  res.status(200).json({ assignments });
+};
 
-exports.inactive_user = async function(req, res){
-  const assignments = await Assignment.find({userId: req.params.uid, active: false});
-  res.status(200).json({assignments});
-}
+exports.inactive_user = async function (req, res) {
+  const assignments = await Assignment.find({
+    userId: req.params.uid,
+    active: false,
+  });
+  res.status(200).json({ assignments });
+};
 
 // exports.create = async function(req, res) {
 //   const assignment = new Assignment({
@@ -50,10 +61,7 @@ exports.inactive_user = async function(req, res){
 //   res.status(200).json({ assignments });
 // };
 
-
-
 // exports.delete = async function(req, res) {
 //   await Assignment.findOneAndDelete(req.params.id);
 //   res.sendStatus(200);
 // };
-
